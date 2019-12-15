415335e5de7a613ea09b129309772118d3baeb8d
Jesper <Jesper.j.m.eriksson@gmail.com>
2019-12-15 04:35:46 +0100

    Add project files.

---
+++ HeatPoint.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace HeatMap_App
+{
+    public class Heat
+    {
+
+        public struct HeatPoint
+        {
+            public int X;
+            public int Y;
+            public byte Intensity;
+            public HeatPoint(int iX, int iY, byte bIntensity)
+            {
+                X = iX;
+                Y = iY;
+                Intensity = bIntensity;
+            }
+        }
+
+        private double ConvertDegreesToRadians(double degrees)
+        {
+            double radians = (Math.PI / 180) * degrees;
+            return (radians);
+        }
+        private void DrawHeatPoint(Graphics Canvas, HeatPoint HeatPoint, int Radius)
+        {
+            // Create points generic list of points to hold circumference points
+            List<Point> CircumferencePointsList = new List<Point>();
+            // Create an empty point to predefine the point struct used in the circumference loop
+            Point CircumferencePoint;
+            // Create an empty array that will be populated with points from the generic list
+            Point[] CircumferencePointsArray;
+            // Calculate ratio to scale byte intensity range from 0-255 to 0-1
+            float fRatio = 1F / Byte.MaxValue;
+            // Precalulate half of byte max value
+            byte bHalf = Byte.MaxValue / 2;
+            // Flip intensity on it's center value from low-high to high-low
+            int iIntensity = (byte)(HeatPoint.Intensity - ((HeatPoint.Intensity - bHalf) * 2));
+            // Store scaled and flipped intensity value for use with gradient center location
+            float fIntensity = iIntensity * fRatio;
+            // Loop through all angles of a circle
+            // Define loop variable as a double to prevent casting in each iteration
+            // Iterate through loop on 10 degree deltas, this can change to improve performance
+            for (double i = 0; i <= 360; i += 10)
+            {
+                // Replace last iteration point with new empty point struct
+                CircumferencePoint = new Point();
+                // Plot new point on the circumference of a circle of the defined radius
+                // Using the point coordinates, radius, and angle
+                // Calculate the position of this iterations point on the circle
+                CircumferencePoint.X = Convert.ToInt32(HeatPoint.X + Radius * Math.Cos(ConvertDegreesToRadians(i)));
+                CircumferencePoint.Y = Convert.ToInt32(HeatPoint.Y + Radius * Math.Sin(ConvertDegreesToRadians(i)));
+                // Add newly plotted circumference point to generic point list
+                CircumferencePointsList.Add(CircumferencePoint);
+            }
+            // Populate empty points system array from generic points array list
+            // Do this to satisfy the datatype of the PathGradientBrush and FillPolygon methods
+            CircumferencePointsArray = CircumferencePointsList.ToArray();
+            // Create new PathGradientBrush to create a radial gradient using the circumference points
+            PathGradientBrush GradientShaper = new PathGradientBrush(CircumferencePointsArray);
+            // Create new color blend to tell the PathGradientBrush what colors to use and where to put them
+            ColorBlend GradientSpecifications = new ColorBlend(3);
+            // Define positions of gradient colors, use intesity to adjust the middle color to
+            // show more mask or less mask
+            GradientSpecifications.Positions = new float[3] { 0, fIntensity, 1 };
+            // Define gradient colors and their alpha values, adjust alpha of gradient colors to match intensity
+            GradientSpecifications.Colors = new Color[3]
+            {
+        Color.FromArgb(0, Color.White),
+        Color.FromArgb(HeatPoint.Intensity, Color.Black),
+        Color.FromArgb(HeatPoint.Intensity, Color.Black)
+            };
+            // Pass off color blend to PathGradientBrush to instruct it how to generate the gradient
+            GradientShaper.InterpolationColors = GradientSpecifications;
+            // Draw polygon (circle) using our point array and gradient brush
+            Canvas.FillPolygon(GradientShaper, CircumferencePointsArray);
+        }
+        private Bitmap CreateIntensityMask(Bitmap bSurface, List<HeatPoint> aHeatPoints)
+        {
+            // Create new graphics surface from memory bitmap
+            Graphics DrawSurface = Graphics.FromImage(bSurface);
+            // Set background color to white so that pixels can be correctly colorized
+            DrawSurface.Clear(System.Drawing.Color.White);
+            // Traverse heat point data and draw masks for each heat point
+            foreach (HeatPoint DataPoint in aHeatPoints)
+            {
+                // Render current heat point on draw surface
+                DrawHeatPoint(DrawSurface, DataPoint, 25);
+            }
+            return bSurface;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+//using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+namespace HeatMap_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class ApexMap
+    {
+        public string MapDir { get; set; }
+        public string MapName { get; set; }
+        public ImageSource Source;
+        public ApexMap(string Dir, string Name)
+        {
+            MapDir = Dir;
+            MapName = Name;
+            Source = new BitmapImage(new Uri("/Resources/" + Dir + ".png", UriKind.Relative));
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public ApexMap KingsC = new ApexMap("KingsC", "Kings Canyon");
+        public ApexMap WorldsE = new ApexMap("WorldE", "World's Edge");
+        public ApexMap Current;
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\.ApexMap\\";
+
+        PixelFormat pf = PixelFormats.Bgr32;
+        static readonly int width = 800;
+        static readonly int height = 800;
+
+        static readonly string OutputName = "Merged";
+        static readonly string outputFileName = AppDomain.CurrentDomain.BaseDirectory + "\\" + OutputName + ".bmp";
+        BitmapImage bitmapImage = new BitmapImage();
+        public MainWindow()
+        {
+            Current = KingsC;
+            InitializeComponent();
+        }
+        public void GenerateHeatMap()
+        {
+            Bitmap bmp = new Bitmap(width, height);
+
+            string Name = "heatmap1";
+            System.Drawing.Color temp;
+
+            if (Current.MapName == "Kings Canyon")
+                temp = System.Drawing.Color.Blue;
+            else
+                temp = System.Drawing.Color.Green;
+
+            for (int i = -100; i < 101; i++) // draw a square
+            {
+                for (int j = -100; j < 101; j++)
+                {
+                    bmp.SetPixel(500 + i, 500 + j, temp);
+                }
+            }
+
+            bmp.Save(AppDomain.CurrentDomain.BaseDirectory + "\\" + Name + ".bmp"); // save "heat"
+
+            Uri uri = new Uri(AppDomain.CurrentDomain.BaseDirectory + "/Resources/" + Current.MapDir + ".png", UriKind.Relative); // load current map
+
+            string stringUri;
+            stringUri = uri.ToString();
+
+            Bitmap bump = ConvertToBitmap((stringUri));  // Convert loaded image
+            Bitmap merged = MergedBitmaps(bmp, bump); // Merge images
+
+            // using streams to avoid read/write issues
+            using (MemoryStream memory = new MemoryStream())
+            {                    
+                    using (FileStream fs = new FileStream(outputFileName, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        merged.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                        byte[] bytes = memory.ToArray();
+                        fs.Write(bytes, 0, bytes.Length);
+                  
+                        var bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        bitmap.StreamSource = fs;
+                        bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmap.EndInit();
+                        ImageOverlay.Source = bitmap;
+                }
+            }
+
+            textboxett.Text = "GenerateHeatMap Done";
+        }
+        public Bitmap ConvertToBitmap(string fileName)
+        {
+            Bitmap bitmap;
+            using (Stream bmpStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            {
+                System.Drawing.Image image = System.Drawing.Image.FromStream(bmpStream);
+
+                bitmap = new Bitmap(image);
+
+            }
+            return bitmap;
+        }
+        private Bitmap MergedBitmaps(Bitmap bmp1, Bitmap bmp2)
+        {
+            Bitmap result = new Bitmap(Math.Max(bmp1.Width, bmp2.Width),
+                                       Math.Max(bmp1.Height, bmp2.Height));
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp2, System.Drawing.Point.Empty);
+                g.DrawImage(bmp1, System.Drawing.Point.Empty);
+            }
+            return result;
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                textboxett.Text = "Starting...";
+
+                switch (Current.MapDir)
+                {
+                    case "KingsC":
+                        Current = WorldsE;
+                        break;
+                    case "WorldE":
+                        Current = KingsC;
+                        break;
+                    default:
+                        break;
+
+                }
+                GenerateHeatMap();
+
+                //ImageView.Source = Current.Source;
+                textboxett.Text = Current.MapName;
+            }
+            catch (FileNotFoundException)
+            {
+                textboxett.Text = "Exception in Button_Click";
+            }
+        }
+    }
+}
+//HeatMap_App.Properties.Resources.WorldE
+//image.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image.png", UriKind.Absolute));
+//ImageOverlay.Source = new BitmapImage(new Uri(path + "myBitmap.png"));
+//ImageOverlay.Source = new BitmapImage(new Uri("C:\\Users\\Joppm\\source\\repos\\HeatMap App\\bin\\Debug\\myBitmap.png"));
+
+//string target = "/Resources/" + Current.MapDir + ".png";
+//ImageView.Source = new BitmapImage(new Uri(target, UriKind.Relative));
+//image.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "image.png", UriKind.Absolute));
